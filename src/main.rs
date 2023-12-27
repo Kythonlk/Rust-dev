a71f0da004e3726a24d6ab599a7ca5d964be9861
kythonlk <kythonlk@gmail.com>
2023-12-27 16:27:38 +0400

    update api endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+extern crate serde;
+extern crate serde_json;
+
+use rocket::serde::{Serialize, json::Json};
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Item {
+    name: String,
+    value: i32,
 }
+
+#[get("/api")] 
+fn get_items() -> Json<Vec<Item>> {
+    let items = vec![
+        Item {
+            name: "Item 1".to_string(),
+            value: 10,
+        },
+        Item {
+            name: "Item 2".to_string(),
+            value: 20,
+        },
+    ];
+
+    Json(items)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![get_items])
+}
+
